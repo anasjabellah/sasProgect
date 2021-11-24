16e3604181e358331f84dd42375a4a96ef52a04d
anas jabellah <jablcodiing@gmail.com>
2021-11-24 09:12:30 +0100

    ajoute fonction

--- index.c
+++ index.c
@@ -117,9 +117,10 @@ int main() {
 
         int menuOperations ;
         returnOperations :
-        printf("1. Retrait \n");
-        printf("2. Depot \n");
-        printf("3. retour a menu \n");
+
+        printf("1. Retrait \n\n");
+        printf("2. Depot \n\n");
+        printf("3. retour a menu \n\n");
 
         printf("donnez choix : ");
         scanf("%d", &menuOperations);
@@ -127,10 +128,12 @@ int main() {
 
         switch (menuOperations){
         case  1:
+            system("cls");
             retirer();
             goto returns;
             break;
         case  2:
+            system("cls");
             Depot();
             goto returns;
             break;
@@ -141,18 +144,21 @@ int main() {
             break;
 
         default:
+            system("cls");
             printf("votre choix doit etre compris entre ( 1 et 3 )  : ");
             goto returnOperations ;
             break;
         };
-    case 4 :
-{
+
+    case 4 :{
+
+        system("cls");
         int Affichage ;
-        printf("1. Par Ordre Ascendant \n");
-        printf("2. Par Ordre Descendant \n");
-        printf("3. Par Ordre Ascendant (les comptes bancaires ayant un montant superieur a un chiffre introduit) \n");
-        printf("4. Par Ordre Descendant (les comptes bancaires ayant un montant superieur a un chiffre introduit) \n");
-        printf("5. retour a menu \n");
+        printf("1. Par Ordre Ascendant \n\n");
+        printf("2. Par Ordre Descendant \n\n");
+        printf("3. Par Ordre Ascendant (les comptes bancaires ayant un montant superieur a un chiffre introduit) \n\n");
+        printf("4. Par Ordre Descendant (les comptes bancaires ayant un montant superieur a un chiffre introduit) \n\n");
+        printf("5. retour a menu \n\n");
 
         printf("donnez choix : ");
         scanf("%d", &Affichage);
@@ -160,6 +166,7 @@ int main() {
         switch (Affichage)
         {
         case 1:{
+            system("cls");
             tempnams();
                 for( i=0 ; i < indexTabel ; i++){
 
@@ -173,18 +180,46 @@ int main() {
             break;
             }
         case 2:
+            system("cls");
             descendants();
+
                 for( i=0 ; i < indexTabel ; i++){
+
                     printf("Client %d:\n",i+1);
                     printf("CIN : %s\n",clit[i].cin);
                     printf("Nom : %s\n",clit[i].nom);
                     printf("Prenom : %s\n",clit[i].prenom);
                     printf("Montant : %d\n",clit[i].montant);
                     printf("\n");
+
                 }
             break;
         
         case 3:
+            system("cls");
+            int nomber_montant ;
+
+            printf("nomber_montant");
+            scanf("%d");
+
+
+            descendants();
+
+                for( i=0 ; i < indexTabel ; i++){
+
+                    if (nomber_montant < clit[i].montant ) 
+                    {
+                        
+                        printf("Client %d:\n",i+1);
+                        printf("CIN : %s\n",clit[i].cin);
+                        printf("Nom : %s\n",clit[i].nom);
+                        printf("Prenom : %s\n",clit[i].prenom);
+                        printf("Montant : %d\n",clit[i].montant);
+                        printf("\n");
+
+                    }
+                    
+                }
             break;
         
         case 4:
@@ -198,7 +233,15 @@ int main() {
         }
  
        break;
-}
+    }
+
+    case 5:
+
+        break;
+    case 6:
+        exit(0);
+        break;
+
     default:
 
         printf("error !! ");
@@ -219,13 +262,13 @@ void titel_bank(){
 };
 
 void meun(){
-    printf("Bonjour qu'est-ce que vous voulez : \n");
-    printf("1 .Creer un compte \n");
-    printf("2 .Introduire plusieur comptes bancaires :\n");
-    printf("3 .Operations :  \n");
-    printf("4 .Affichage  \n");
-    printf("5 .Fidelisation  \n");
-    printf("6 .Quitter  \n");
+    printf("Bonjour qu'est-ce que vous voulez : \n\n");
+    printf("1 .Creer un compte \n\n");
+    printf("2 .Introduire plusieur comptes bancaires :\n\n");
+    printf("3 .Operations :  \n\n");
+    printf("4 .Affichage  \n\n");
+    printf("5 .Fidelisation  \n\n");
+    printf("6 .Quitter  \n\n");
 }
 
 void retirer(){
@@ -321,11 +364,11 @@ void descendants(){
         for (j=0 ; j < indexTabel ; j++)
         
         
-            if (clit[i].montant < clit[j].montant ) {
+            if (clit[i].montant > clit[j].montant ) {
 
                 temporaire = clit[i];
                 clit[i] = clit[j];
                 clit[j] = temporaire ;
             }
 }
-/* ======================================= end fontion void ==================================  */  
+/* ======================================= end fontion void ==================================  */
